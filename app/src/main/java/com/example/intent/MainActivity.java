850e1a972a9dc758e8056ad4dca7661225e9f863
I Gede Surya Rahayuda <surya.rahayuda@gmail.com>
2023-03-11 22:33:44 +0800

    First Git

---
+++ app/src/main/java/com/example/intent/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.intent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void panggil(View view) {
+        String nomor = "0361422001" ;
+        Intent panggil = new Intent(Intent. ACTION_DIAL);
+        panggil.setData(Uri. fromParts("tel",nomor,null));
+        startActivity(panggil);
+    }
+    public void buka(View view) {
+        String url = "https://handwritten-recognition.000webhostapp.com/" ;
+        Intent bukabrowser = new Intent(Intent. ACTION_VIEW);
+        bukabrowser.setData(Uri. parse(url));
+        startActivity(bukabrowser);
+    }
+    public void tentang(View view) {
+        Intent tentang = new Intent(MainActivity.this, Tentang.class);
+        startActivity(tentang);
+    }
+    public void clear(View view) {
+        Intent clear = new Intent(MainActivity.this, Clear.class);
+        startActivity(clear);
+    }
+}
